ec54e2c80c57faabc31620071507345cef9baf63
Jatin498 <140939291+Jatin498@users.noreply.github.com>
2023-10-08 12:07:51 +0530

    Update frn.py

--- ILovePDF/lang/frn.py
+++ ILovePDF/lang/frn.py
@@ -2,7 +2,7 @@
 # Feel free to use and contribute to this project. Your contributions are welcome!
 # copyright ©️ 2021 nabilanavab
 
-file_name = "ILovePDF/lang/frn.py"
+file_name = "@AdultManhwa_nation/lang/frn.py"
 
 from configs.config   import settings
 
